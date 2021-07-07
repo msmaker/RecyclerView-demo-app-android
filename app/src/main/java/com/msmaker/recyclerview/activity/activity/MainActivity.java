3845e8db91b2d92e811b65f74849a60442ca332b
Marco Silva <marcosilva.tec@gamil.com>
2021-07-07 15:59:54 -0300

    Adapter

---
+++ app/src/main/java/com/msmaker/recyclerview/activity/activity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.msmaker.recyclerview.activity.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.msmaker.recyclerview.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Configurar adapter
+
+        //Configurar RecyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
+        //recyclerView.setAdapter();
+    }
+}
